b27e1ff3f98cce8c478f18bff2a0981fc99c1503
ColdCoder92 <lucasrodriguez1002@proton.me>
2025-08-30 18:36:56 -0400

    Complete Nav Bar, Footer, and Home Page

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import NavBar from './shared/NavBar'
+import { Container, Grid } from '@mui/material'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      <NavBar />
+      <h1>Projects</h1>
+      <footer className="footer">
+        <Container>
+          <Grid container spacing={2}>
+            <Grid size={6}>
+              <p>&copy; 2025 Lucas Rodriguez</p>
+            </Grid>
+            <Grid size={6}>
+                <a href="https://www.linkedin.com/in/lucas-rodriguez-coder/" target="_blank">
+                    <img className="logo" src="src/assets/linkedinBlackLogo.png" alt="LinkedIn Icon" />
+                </a>
+                <a href="https://github.com/ColdCoder92" target="_blank">
+                    <img className="logo" src="src/assets/githubBlackLogo.png" alt="GitHub Icon" />
+                </a>
+                <a href="mailto:lucasrodriguez1002@proton.me">
+                    <img className="logo" src="src/assets/emailBlackLogo.png" alt="Email Icon" />
+                </a>
+            </Grid>
+          </Grid>
+        </Container>
+      </footer>
+    </div>
   )
 }
 
