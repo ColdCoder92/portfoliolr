ead8fd9f2b67d74a3206a10edafb3bec7b936d48
ColdCoder92 <lucasrodriguez1002@proton.me>
2025-10-06 19:31:24 -0400

    Apply Changes to Working Branch

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {HashRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfoliolr">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
